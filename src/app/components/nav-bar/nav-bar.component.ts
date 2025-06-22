328b5695ea400bbacd2b31ef88b5dadd9bbcef95
Adam <adamzambudiooo@gmail.com>
2025-06-23 01:48:39 +0200

    ultimos arreglos responsive servidor

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,12 +11,14 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  @ViewChild('navbarCollapse', { static: false }) navbarCollapse!: ElementRef;
 
-  constructor(public authService: AuthService, private router: Router) {}
+  constructor(public authService: AuthService, private router: Router, private toastService: ToastService) {}
 
 
   logout() {
     this.authService.logout();
+    this.toastService.success('Sesión cerrada correctamente.');
     this.router.navigate(['/dashboard']);
   }
 
@@ -31,4 +34,9 @@ export class NavBarComponent {
     this.router.navigate(['/profile']); // Cambia la ruta si tienes otro componente
   }
 
+  closeNavbar() {
+    if (this.navbarCollapse?.nativeElement?.classList.contains('show')) {
+      this.navbarCollapse.nativeElement.classList.remove('show');
+    }
+  }
 }
